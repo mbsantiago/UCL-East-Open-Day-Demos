02ddd4d29a8b5ca2820ae93e7df8120d29421038
Santiago Martinez <santiago.mbal@gmail.com>
2023-09-15 15:07:49 +0100

    made batdetect2 work

--- batdetect2/process.py
+++ batdetect2/process.py
@@ -2,22 +2,24 @@
 from multiprocessing import Queue
 
 import numpy as np
-from batdetect2 import api
 
 
-def process(audio: np.ndarray):
+def process(audio: np.ndarray, api):
     """Process audio buffer.
 
     Will analyze it using the birdnetlib analyzer.
     """
     audio = audio.astype(np.float32) / np.iinfo(np.int16).max
     detections, _, _ = api.process_audio(audio)
+    detections = []
     return audio, detections
 
 
 def process_audio(input_queue: Queue, ouput_queue: Queue):
     """Process that continuously processes audio buffers."""
+    from batdetect2 import api
+
     while True:
         audio = input_queue.get()
-        audio, detections = process(audio)
+        audio, detections = process(audio, api)
         ouput_queue.put([audio, detections])
